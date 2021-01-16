cb900752b4825069efb3e6ef4f5c83bc412ffdcc
Glauco <glauco@iMac-de-Glauco.local>
2021-01-16 13:09:12 -0300

    criando estruturas de pastas

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import '../lib/main.dart';
 
-import 'package:shop_app/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
