bb8afe183ddf1cef996db33e458bca921af77d5b
Glauco <glauco@iMac-de-Glauco.local>
2021-01-16 13:55:43 -0300

    criando class product

--- lib/views/product_overview_screen.dart
+++ lib/views/product_overview_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+import '../data/dummy_data.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> leadedProducts = DUMMY_PRODUCTS;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2),
+        itemBuilder: (context, index){
+          return Text(leadedProducts[index].title);
+        },
+      ),
     );
   }
 }
